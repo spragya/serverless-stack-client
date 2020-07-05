57dafb81c0d8aab3b3ee348791351c340fa1574d
Pragya Srivastava <pragya.srivastava@alchemysystems.com>
2020-07-05 18:25:38 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "pragya-notes-app-uploads",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://i5wy58dve3.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_QBpfcN9gq",
+    APP_CLIENT_ID: "2v5ceclo4o5ll4be9e201t0mp9",
+    IDENTITY_POOL_ID: "us-east-1:860f5a76-ba80-4f2d-9f62-17a075743856",
+  },
+};
